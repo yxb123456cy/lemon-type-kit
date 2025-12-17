5415ddaff33044bf3d67436665d96ae9e07dfdf8
yxb123456cy <2186256471@qq.com>
2025-12-17 19:45:50 +0800

    feat(kits): :sparkles: 新增date相关函数 getCurrentYear、getCurrentDate

---
+++ src/date/__tests__/index.test.ts
@@ -0,0 +1,26 @@
+import { describe, expect, it } from 'vitest';
+import { getCurrentDate, getCurrentYear } from '..';
+
+describe('date utils', () => {
+  describe('getCurrentYear', () => {
+    it('should return current year as number', () => {
+      const date = getCurrentYear();
+      expect(date).toBeTypeOf('number');
+    });
+    it('should return current year as string', () => {
+      const date = getCurrentYear(true);
+      expect(date).toBeTypeOf('string');
+    });
+    it('should return value is current year', () => {
+      const date = getCurrentYear();
+      expect(date).toBe(2025);
+    });
+  });
+
+  describe('getCurrentDate', () => {
+    it('should return current date', () => {
+      const date = getCurrentDate();
+      expect(date).toBeInstanceOf(Date);
+    });
+  });
+});
---
+++ src/date/index.ts
@@ -0,0 +1,16 @@
+/**
+ * 获取当前年份
+ * @param returnString 是否返回字符串类型（默认false）
+ * @returns 当前年份（数字/字符串）
+ */
+export function getCurrentYear(returnString: boolean = false): string | number {
+  const year = new Date().getFullYear();
+  return returnString ? year.toString() : year;
+}
+/**
+ * 获取当前日期的Date对象
+ * @returns 当前日期的Date实例
+ */
+export function getCurrentDate(): Date {
+  return new Date();
+}
